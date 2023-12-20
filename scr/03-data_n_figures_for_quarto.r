3540331fb5158141860b0b0703f23b7b1e72405f
Claire <clrccljuhasz@gmail.com>
2023-12-20 12:25:17 -0500

    ajout de la figure SPI-occ

--- scr/03-data_n_figures_for_quarto.r
+++ scr/03-data_n_figures_for_quarto.r
@@ -23,7 +23,7 @@ aires_latlon$POPINFOS <- paste0(
         "</b><br> superficie: <b>", round(aires_latlon$HA_LEGAL, digit = 1), " ha </b>")
 
 # ----------------------------------------------------------------------- #
-#### figure 2 - courbes de tendance de SPI à partir aire distribution ####
+#### figure 2A - courbes de tendance de SPI à partir aire distribution ####
 # --------------------------------------------------------------------- #
 
 SPI <- read.csv("results/SPI.csv")[, -1]
@@ -99,3 +99,58 @@ sspi_df$GROUPE[is.na(sspi_df$GROUPE)] <- "other"
 #         )
 #     )
 # ) 
+
+# ----------------------------------------------------------------------- #
+#### figure 2B - courbes de tendance de SPI à partir aire distribution ####
+# --------------------------------------------------------------------- #
+
+SOCC <- read.csv("results/SPI_OCC.csv")
+SOCC <- SOCC[!(SOCC$SPECIES == "Information masquée"),]
+species <- as.character(unique(SOCC$SPECIES))
+years <- as.numeric(unique(SOCC$YEAR))
+
+# smoothing the curves
+spi_ls <- split(SOCC, SOCC$SPECIES)
+
+spi_smoo <- lapply(spi_ls, function(x){
+    m <- cbind(x$YEAR, x$SPI)
+    sm <- smooth_ksmooth(m, smoothness = 4)
+    df <- as.data.frame(sm)
+    names(df) <- c("YEAR", "SPI")
+    df$SPECIES <- unique(x$SPECIES)
+    df$POPINFOS <- paste0(
+        "<b>", df$SPECIES,
+        "</b><br>Année <b>", floor(df$YEAR),
+        "</b><br>SPI = <b>", round(df$SPI, digits = 3), "</b>")
+    df
+})
+
+sspi_df_occ <- do.call("rbind", spi_smoo)
+
+# computation of mean SPI
+year_mean <- c()
+for (i in years) {
+    sub_year <- SOCC$SPI[SOCC$YEAR == i]
+    year_mean <- c(year_mean, mean(sub_year, na.rm = TRUE))
+}
+s_mean_spi_occ <- smooth_ksmooth(as.matrix(cbind(years, year_mean)))
+    df <- as.data.frame(s_mean_spi_occ)
+    names(df) <- c("YEAR", "SPI")
+    df$SPECIES <- "Valeur moyenne"
+    df$POPINFOS <- paste0(
+        "<b>", df$SPECIES,
+        "</b><br>Année <b>", floor(df$YEAR),
+        "</b><br>SPI = <b>", round(df$SPI, digits = 3), "</b>")
+
+sspi_df_occ <- rbind(sspi_df_occ, df)
+
+# Association d'un groupe par catégories - max, min & mean 
+spi2023 <- SOCC[SOCC$YEAR == 2023,]
+range(spi2023$SPI)
+max_spe <- spi2023$SPECIES[spi2023$SPI == max(spi2023$SPI)]
+min_spe <- spi2023$SPECIES[spi2023$SPI == min(spi2023$SPI)]
+
+sspi_df_occ$GROUPE[sspi_df_occ$SPECIES == "Valeur moyenne"] <- "mean"
+sspi_df_occ$GROUPE[sspi_df_occ$SPECIES == max_spe] <- "max"
+sspi_df_occ$GROUPE[sspi_df_occ$SPECIES %in% min_spe] <- "min"
+sspi_df_occ$GROUPE[is.na(sspi_df_occ$GROUPE)] <- "other"
